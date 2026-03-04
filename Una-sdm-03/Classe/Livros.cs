9bc578ee892958f0eebb7672d66e271c38553919
Samuel-Soares755 <samu123soares@gmail.com>
2026-03-03 21:55:08 -0300

    Commit 03

---
+++ Una-sdm-03/Classe/Livros.cs
@@ -0,0 +1,9 @@
+
+public class Livro
+{
+    public string titulo { get; set; }
+    public string autor { get; set; }
+    public int anoPublicacao { get; set; }
+    public string genero { get; set; }
+    public bool disponivel { get; set; }
+}
---
+++ Una-sdm-03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+class Program
+{
+    static void Main()
+    {
+        string json = File.ReadAllText("livro.json");
+
+        Livro livro = JsonSerializer.Deserialize<Livro>(json);
+
+        Console.WriteLine("Livro carregado:");
+        Console.WriteLine($"Título: {livro.titulo}");
+        Console.WriteLine($"Autor: {livro.autor}");
+        Console.WriteLine($"Ano: {livro.anoPublicacao}");
+        Console.WriteLine($"Disponível: {livro.disponivel}");
+    }
+}
